6ed544f10789d0cc897cdb0ba31dd5d285c1ae4e
jinto.sunouchi <board.jin@gmail.com>
2019-10-31 06:54:33 +0900

    Add ShowServlet

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -33,7 +33,6 @@ public class CreateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         String _token = (String)request.getParameter("_token");
-        System.out.println("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
